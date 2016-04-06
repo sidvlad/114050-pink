e2fcafdd81b31c6293137d91b3c323c1e0ae26f2
Bogov Vladimir <bogov.v.m@gmail.com>
2016-04-06 15:39:25 +0200

    npm run buld now works

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,113 @@
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync");
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var server = require('browser-sync');
+var mqpacker = require('css-mqpacker');
+var minify = require('gulp-csso');
+var rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+var svgmin = require('gulp-svgmin');
+var copy = require('gulp-contrib-copy');
+var clean = require('gulp-contrib-clean');
 
-gulp.task("style", function() {
-  gulp.src("sass/style.scss")
+gulp.task('style', function() {
+  gulp.src('sass/style.scss')
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
       autoprefixer({browsers: [
-        "last 1 version",
-        "last 2 Chrome versions",
-        "last 2 Firefox versions",
-        "last 2 Opera versions",
-        "last 2 Edge versions"
-      ]})
+        'last 1 version',
+        'last 2 Chrome versions',
+        'last 2 Firefox versions',
+        'last 2 Opera versions',
+        'last 2 Edge versions'
+      ]}),
+      mqpacker({
+        sort: true
+      })
     ]))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest('build/css'))
+    .pipe(minify())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css'))
     .pipe(server.reload({stream: true}));
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task('images', function() {
+  return gulp.src('img/**/*.{png,jpg,gif}')
+  .pipe(imagemin({
+    optimizationLevel: 5,
+    progressive: true
+    }))
+  .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('symbols', function() {
+  return gulp.src('img/*.svg')
+  .pipe(svgmin())
+  .pipe(gulp.dest('build/img'))
+});
+
+gulp.task('fonts', function() {
+  gulp.src('fonts/**/*.{woff,woff2}')
+    .pipe(gulp.dest('build/fonts'));
+});
+
+gulp.task('script', function() {
+  gulp.src('js/**/*.js')
+    .pipe(gulp.dest('build/js'))
+    .pipe(server.reload({stream: true}));
+});
+
+gulp.task('html', function() {
+  gulp.src('*.html')
+    .pipe(gulp.dest('build'))
+    .pipe(server.reload({stream: true}));
+});
+
+gulp.task('copy', function() {
+  gulp.src('**/*')
+    .pipe(copy())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function() {
+  gulp.src('build, {read: false}')
+    .pipe(clean());
+});
+
+gulp.task('clean', function() {
+  gulp.src('build')
+    .pipe(clean());
+});
+
+gulp.task('build', ['clean'], function() {
+  gulp.start(
+    'style',
+    'fonts',
+    'html',
+    'script',
+    'images',
+    'symbols'
+  );
+});
+
+gulp.task('serve', ['style'], function() {
   server.init({
-    server: ".",
+    server: 'build',
     notify: false,
     open: true,
     ui: false
   });
 
-  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+  gulp.watch('sass/**/*.{scss,sass}', ['style']);
+  // gulp.watch("*.html").on("change", server.reload);
+  gulp.watch('*.html', ['html']).on("change", server.reload);
+  gulp.watch('js/**/*.js', ['script']).on("change", server.reload);
+  gulp.watch('img/!**!/!*.{png,jpg,gif,svg}', ['images']).on("change", server.reload);
+  gulp.watch('fonts/!**/!*.{woff,woff2}', ['fonts']).on("change", server.reload);
 });
